3ae7542e85d4e4121f642b76108f3ed800b96a27
geek007 <basit.raza11@gmail.com>
2016-11-06 08:11:17 -0500

    Code formatting

--- index.php
+++ index.php
@@ -8,9 +8,6 @@ $app = new Slim\App();
 
 $app->post('/export', function ($req, $res) {
 
-       // Open the output stream
-       //$fs = fopen('php://output', 'w');
-
        // Start output buffering (to capture stream contents)
        ob_start();
        $tokenizer = new WhitespaceAndPunctuationTokenizer();       
@@ -19,50 +16,37 @@ $app->post('/export', function ($req, $res) {
        
        $text = $request['text'];
        $tokens = $tokenizer->tokenize($text);
-       //print_r($token);	
        $tags = $request['tags'];
        $fileContent = "";
+       
        foreach($tokens as $token){
           $fileContent .= $token."\t";
-	  $text = 'O';
-	foreach($tags as $tag){
-          if($tag['word'] == $token ) {
-             $text = $tag['tag'];
-	     break;
-		
-          }
-	}
-	$fileContent .= $text;
+        	$text = 'O';
+        	foreach($tags as $tag){
+                  if($tag['word'] == $token ) {
+                     $text = $tag['tag'];
+        	     break;
+        		
+                  }
+        	}
+        	$fileContent .= $text;
           $fileContent .= "\n";
+        }
 
-       }
-
-        //fwrite($fs, $fileContent);
-	//$string = ob_get_clean();
-	header('Content-Description: File Transfer');
+        header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-	header("Content-Type: application/force-download", FALSE);
-	header("Content-Type: application/download", FALSE);
-	header('Content-Disposition: attachment;filename="'.$filename.'"');
-	header('Content-Transfer-Encoding: Binary');
-	header('Pragma: public');	
+      	header("Content-Type: application/force-download", FALSE);
+      	header("Content-Type: application/download", FALSE);
+      	header('Content-Disposition: attachment;filename="'.$filename.'"');
+      	header('Content-Transfer-Encoding: Binary');
+      	header('Pragma: public');	
         header('Content-Length: '.strlen($fileContent));
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
         header('Expires: 0');
-	//readfile($string);
-    //ob_clean();    
-    //flush();
-    //readfile('php://output');    
-    //exit();
-
-        //exit($string);
-	echo $fileContent;
-        //exit;
-
-	//$res->write($fileContent);
+	      echo $fileContent;
+        
 });
 
 
 
 $app->run();
-
